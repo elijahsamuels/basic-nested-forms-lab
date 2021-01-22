0abefe8d6fd741db5adcdb6acd6c6fc4057a29dd
elijahsamuels <elijahsamuels@gmail.com>
2021-01-22 12:35:26 -0700

    commit tree a738bd0789418cdb199129db286fa758db62befe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # def ingredient_params
+  #   params.require(:recipe).permit(:name, :quantity)
+  # end
+
+
 end
